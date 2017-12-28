23a8df453bae8fbd1c76437af6999cbd34eca2a9
Phuong Nguyen <phuong.nguyen@iit.it>
2017-12-28 11:41:46 +0700

    [dataset] add script to download sim2real dataset

---
+++ download_sim2real_dataset.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+mkdir datasets
+FILE=sim2real
+
+URL=https://zenodo.org/record/1133474/files/sim2real.tar.gz
+ZIP_FILE=./datasets/$FILE.tar.gz
+TARGET_DIR=./datasets/$FILE/
+wget -N $URL -O $ZIP_FILE
+#mkdir $TARGET_DIR
+#unzip $ZIP_FILE -d ./datasets/
+tar -xvzf $ZIP_FILE -C ./datasets/
+rm $ZIP_FILE
