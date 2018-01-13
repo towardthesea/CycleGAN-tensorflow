57826f0bb5cb90f7604c124c55556926d3b4b73d
Phuong Nguyen <phuong.nguyen@iit.it>
2018-01-14 00:49:29 +0700

    [dataset] update dataset link with mask

--- download_sim2real_dataset.sh
+++ download_sim2real_dataset.sh
@@ -2,7 +2,7 @@
 mkdir datasets
 FILE=sim2real
 
-URL=https://zenodo.org/record/1133474/files/sim2real.tar.gz
+URL=https://zenodo.org/record/1146867/files/sim2real.tar.gz
 ZIP_FILE=./datasets/$FILE.tar.gz
 TARGET_DIR=./datasets/$FILE/
 wget -N $URL -O $ZIP_FILE
