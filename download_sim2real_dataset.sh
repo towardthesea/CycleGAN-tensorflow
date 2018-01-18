cd5bd12589738606ada8008ba53d5326c21b4842
Phuong Nguyen <phuong.nguyen@iit.it>
2018-01-18 14:13:50 +0100

    [dataset] fix bug in download script

--- download_sim2real_dataset.sh
+++ download_sim2real_dataset.sh
@@ -6,7 +6,7 @@ URL=https://zenodo.org/record/1146867/files/sim2real.tar.gz
 ZIP_FILE=./datasets/$FILE.tar.gz
 TARGET_DIR=./datasets/$FILE/
 wget -N $URL -O $ZIP_FILE
-#mkdir $TARGET_DIR
+mkdir $TARGET_DIR
 #unzip $ZIP_FILE -d ./datasets/
 tar -xvzf $ZIP_FILE -C $TARGET_DIR
 rm $ZIP_FILE
